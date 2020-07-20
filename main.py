ede6101a10c0c7b0369e7391a52ffbb12c4f8a66
Fernando de Almeida Noronha <30605367+ferNoronha@users.noreply.github.com>
2020-07-20 08:57:29 -0300

    teste

--- main.py
+++ main.py
@@ -1,25 +1,27 @@
 from fastapi import FastAPI, File, Body
-#from pallet import Pallet
-#import base64
-#import cv2
-#from pydantic import BaseModel
+from pallet import Pallet
+import base64
+import cv2
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
-#class Item(BaseModel):
-#    data: str
+class Item(BaseModel):
+    data: str
 
 #data: str = Body(...)
-#@app.post("/clustering/")
-# async def process(item: Item):
-#     print(type(item.data))
-#     colors = Pallet()
-#     file64 = item.data.split(',')[1].encode()
-#     b64_string = file64.decode()
-#     centers,error = colors.process(b64_string)
-#     error = []
-#     return {"message":centers, "errors":error}
+@app.post("/clustering/")
+async def process(item: Item):
+    #centers = "teste"
+    #error = 'testado'
+    print(type(item.data))
+    colors = Pallet()
+    file64 = item.data.split(',')[1].encode()
+    b64_string = file64.decode()
+    centers,error = colors.process(b64_string)
+    error = []
+    return {"message":centers, "errors":error}
 
 @app.get("/")
 async def root():
